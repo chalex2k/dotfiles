f8413bbd3e9a79a01320dc0371f99b15e02e6bb7
chalex2k <chalex2k@gmail.com>
2026-07-27 00:41:01 +0300

    Add Neovim file tree plugin

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -2,3 +2,25 @@ vim.opt.number = true
 vim.opt.relativenumber = true
 vim.cmd.colorscheme("habamax")
 
+vim.g.mapleader = " "
+
+vim.pack.add({
+  "https://github.com/nvim-tree/nvim-web-devicons",
+  "https://github.com/nvim-tree/nvim-tree.lua",
+})
+
+require("nvim-tree").setup({
+  view = {
+    width = 36,
+  },
+  renderer = {
+    group_empty = true,
+  },
+  filters = {
+    dotfiles = false,
+  },
+})
+
+vim.keymap.set("n", "<leader>e", "<cmd>NvimTreeToggle<cr>", {
+  desc = "Toggle project file tree",
+})
